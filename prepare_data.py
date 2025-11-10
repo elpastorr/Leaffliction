d101100e2227d02b88753e60ad87cabd167d447c
Elio PASTORET <elpastor@f2r2s12.clusters.42paris.fr>
2025-11-10 17:41:40 +0100

    prepare_data fix

--- prepare_data.py
+++ prepare_data.py
@@ -9,7 +9,7 @@ import argparse
 
 def split_data(source_dir):
     """
-    Split images from src dir into training (80%) and validation (20%) sets.
+    Split images from src dir into training (90%) and validation (10%) sets.
 
     Args:
         source_dir (str): Path to the source directory containing images
@@ -34,7 +34,7 @@ def split_data(source_dir):
 
     random.shuffle(image_files)
 
-    split_index = int(len(image_files) * 0.8)
+    split_index = int(len(image_files) * 0.9)
 
     # Split files into training and validation sets
     train_files = image_files[:split_index]
@@ -65,7 +65,7 @@ def split_data(source_dir):
         dst_path = os.path.join(class_dir, os.path.basename(file))
         shutil.copy2(file, dst_path)
 
-    print("Séparation terminée : ~80% in training, ~20% in validation :")
+    print("Séparation terminée : ~90% in training, ~10% in validation :")
     print(f"{len(train_files)} files in training, {len(val_files)}" +
           " files in validation.")
 
@@ -104,18 +104,16 @@ def main():
         if args.a or args.t:
             if not os.path.exists(dst_dataset[0]):
                 os.makedirs(dst_dataset[0])
-            if not os.path.exists(dst_dataset[1]):
-                os.makedirs(dst_dataset[1])
         
             if args.a:
                 augment_dir(src_dataset[0], dst_dataset[0])
             if args.t:
                 process_directory(src_dataset[0], dst_dataset[0])
 
-            shutil.copy2(src_dataset[1], dst_dataset[1])
+            shutil.copytree(src_dataset[1], dst_dataset[1])
+            shutil.rmtree("splited_images")
 
         print("Created prepared dataset in 'prepared_dataset' directory.")
-        shutil.rmtree("splited_images")
 
     except FileNotFoundError as e:
         print("Error:", e, file=sys.stderr)
