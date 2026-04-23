3ac359d6d9581eb315a842b18bdbd49028bd3e31
elpastorr <89456448+elpastorr@users.noreply.github.com>
2026-04-23 19:02:34 +0200

    Better init.sh
    Updated init.sh to provide feedback when creating a virtual environment and when installing requirements. It now checks for the existence of requirements.txt before attempting to install.

--- init.sh
+++ init.sh
@@ -7,9 +7,17 @@ if [ ! -d images ]; then
 fi
 
 if [ ! -d .venv ]; then
+    echo "Creating virtual env..."
     python3 -m venv .venv
 fi
 
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+.venv/bin/pip install --upgrade pip
+
+if [ -f "requirements.txt" ]; then
+    .venv/bin/pip install -r requirements.txt
+else
+    echo "No requirements.txt found"
+fi
+
+echo "Requirements installed now activate venv with:"
+echo "source .venv/bin/activate"
